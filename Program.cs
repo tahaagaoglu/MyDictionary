f6d32f19f1e55ee28b9e8b40ff071547c9a07468
tahaga61 <tahaga61@gmail.com>
2021-02-03 02:17:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary <int, string>();
+            dictionary.Add(34, "İSTANBUL");
+            dictionary.Add(35,"İZMİR");
+            dictionary.Add(01, "ANKARA");
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
